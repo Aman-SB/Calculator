422bc6d94c40b436c2fc0e55f2638cbf71aa707f
Aman Bisht <bishtaman60@gmail.com>
2023-11-18 20:43:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Calculator from "./Components/Calculator";
+
+import "./style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
